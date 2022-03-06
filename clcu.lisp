6fcb9d5afab33b724abbc9adc8ea55c29840e9da
Jay Morgan <jaymiles17@gmail.com>
2022-03-06 13:06:59 +0100

    Add string-split

--- clcu.lisp
+++ clcu.lisp
@@ -1,4 +1,4 @@
-;;;; clc.lisp
+;;;; clcu.lisp
 
 (in-package #:clcu)
 
@@ -11,3 +11,12 @@
 (defun compose (&rest funs)
   "Compose many functions into a single function"
   (lambda (in) (apply #'-> in funs)))
+
+(defun string-split (str &optional (delim " "))
+  "Split a string into a sequence, splitting on a delimiter."
+  (labels ((f (s r)
+	     (let ((pos (search delim s)))
+	       (cond ((not (eq nil pos)) (f (subseq s (1+ pos)) (cons (subseq s 0 pos) r)))
+		     ((string= s "") (nreverse r))
+		     (t (nreverse (cons s r)))))))
+    (f str nil)))
